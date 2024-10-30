8cb30c4b03bd637eb815089f1cfe4d46cd88c4a8
miavai649 <mahmudulhaquenoor@gmail.com>
2024-10-30 19:56:07 +0600

    created a new brance called miavai and restructure the file structure

--- src/app.ts
+++ src/app.ts
@@ -1,28 +1,28 @@
-import express, { Application, Request, Response } from "express";
-import cors from "cors";
-import cookieParser from "cookie-parser";
-import globalErrorHandler from "./middlewares/globalErrorHandler";
-import notFound from "./middlewares/notFound";
+import express, { Application, Request, Response } from 'express'
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
+import globalErrorHandler from './app/middlewares/globalErrorHandler'
+import notFound from './app/middlewares/notFound'
 
-const app: Application = express();
+const app: Application = express()
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:3000"],
-    credentials: true,
+    origin: ['http://localhost:5173', 'http://localhost:3000'],
+    credentials: true
   })
-);
+)
 
-app.use(express.json());
-app.use(cookieParser());
+app.use(express.json())
+app.use(cookieParser())
 
 // app.use("/api/v1", router);
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello Next Level Developers 👋!!!");
-});
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello Next Level Developers 👋!!!')
+})
 
-app.use(globalErrorHandler);
-app.use(notFound);
+app.use(globalErrorHandler)
+app.use(notFound)
 
-export default app;
+export default app
