b620093aaf5d61f0c1f67688e5fa2a29ebfe9de2
miavai649 <mahmudulhaquenoor@gmail.com>
2024-10-31 12:07:53 +0600

    update user service function created

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,47 +1,60 @@
-import catchAsync from "../../utils/catchAsync";
-import SendResponse from "../../utils/sendResponse";
-import httpStatus from "http-status";
-import { UserServices } from "./user.service";
+import catchAsync from '../../utils/catchAsync'
+import SendResponse from '../../utils/sendResponse'
+import httpStatus from 'http-status'
+import { UserServices } from './user.service'
 
 const createUser = catchAsync(async (req, res) => {
-  const userData = req.body;
+  const userData = req.body
 
-  const result = await UserServices.createUserIntoDb(userData);
+  const result = await UserServices.createUserIntoDb(userData)
 
   SendResponse(res, {
     success: true,
     statusCode: httpStatus.CREATED,
-    message: "User Created Successfully",
-    data: result,
-  });
-});
+    message: 'User Created Successfully',
+    data: result
+  })
+})
 
 const getAllUser = catchAsync(async (req, res) => {
-  const result = await UserServices.getAllUsersFromDb();
+  const result = await UserServices.getAllUsersFromDb()
 
   SendResponse(res, {
     success: true,
     statusCode: httpStatus.OK,
-    message: "Users retrieved Successfully",
-    data: result,
-  });
-});
+    message: 'Users retrieved Successfully',
+    data: result
+  })
+})
 
 const getUser = catchAsync(async (req, res) => {
-  const { id } = req.params;
+  const { id } = req.params
 
-  const result = await UserServices.getUserFromDb(id);
+  const result = await UserServices.getUserFromDb(id)
 
   SendResponse(res, {
     success: true,
     statusCode: httpStatus.OK,
-    message: "User retrieved Successfully",
-    data: result,
-  });
-});
+    message: 'User retrieved Successfully',
+    data: result
+  })
+})
+
+// const updateUser = catchAsync(async (req, res) => {
+//   const {}
+
+//   const result = await UserServices.getUserFromDb(id);
+
+//   SendResponse(res, {
+//     success: true,
+//     statusCode: httpStatus.OK,
+//     message: "User retrieved Successfully",
+//     data: result,
+//   });
+// });
 
 export const UserControllers = {
   createUser,
   getAllUser,
-  getUser,
-};
+  getUser
+}
